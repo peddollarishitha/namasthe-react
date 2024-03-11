be8ea3207606788372d1e78bbd36fbde4ea7c130
peddollarishitha <peddollarishitha002@gmail.com>
2024-03-11 14:37:33 +0530

    ep3

---
+++ App.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", {id: "parent"}, 
+    React.createElement("div", {id: "child"}, 
+        React.createElement("h1", {id: "heading"}, "I'm a heading")
+    )
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
